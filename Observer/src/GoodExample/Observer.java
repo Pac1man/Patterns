bac179f97cc72fb8894c29ff6596a23e5c2d3d7d
Daniil Baev <pingman772@gmail.com>
2019-02-25 18:30:00 +0300

    New Patterns

---
+++ Observer/src/GoodExample/Observer.java
@@ -0,0 +1,5 @@
+package GoodExample;
+
+public interface Observer {
+    public void update(float temp, float humidity, float pressure);
+}
---
+++ Observer/src/GoodExample/Subject.java
@@ -0,0 +1,8 @@
+package GoodExample;
+
+public interface Subject {
+    public void registerObserver(Observer o);
+    public void removeObserver(Observer o);
+    public void notifyObservers();
+
+}
