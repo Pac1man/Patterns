b0a3791457bf441a5e3fdd72384f9fe7f3aba454
Daniil Baev <pingman772@gmail.com>
2019-03-01 09:53:30 +0300

    New pattern Observer

---
+++ Observer/src/OwnExample/Newspaper.java
@@ -0,0 +1,14 @@
+package OwnExample;
+
+
+public class Newspaper{
+    public static void main(String[] args) {
+        Subscribers subscribers = new Subscribers();
+        Updater updater = new Updater(subscribers);
+        subscribers.setSubscribers(3, "Magnitogorsk");
+        subscribers.setSubscribers(10, "Kazan");
+        subscribers.setSubscribers(200, "Moscow");
+    }
+
+
+}
---
+++ Observer/src/WithUtil/WeatherStation.java
@@ -0,0 +1,14 @@
+package WithUtil;
+
+import GoodExample.CurrentConditionalDisplay;
+import GoodExample.WeatherData;
+
+public class WeatherStation {
+    public static void main(String[] args) {
+        WeatherData weatherData = new WeatherData();
+        CurrentConditionalDisplay currentDisplay = new CurrentConditionalDisplay(weatherData);
+        weatherData.setMeasurements(80, 65, 30.f);
+        weatherData.setMeasurements(82, 70,29.2f);
+        weatherData.setMeasurements(78, 90, 29.2f);
+    }
+}
