e1501958468444f952f2c0e46935850cb915d425
Daniil Baev <pingman772@gmail.com>
2019-03-01 19:37:43 +0300

    Починил анархию в папках

---
+++ src/Adapter/GoodExample/TurkeyAdapter.java
@@ -0,0 +1,13 @@
+package Adapter.GoodExample;
+
+
+import Strategy.BadExmaple.RedheadDuck;
+import Strategy.GoodExample.*;
+
+public class TurkeyAdapter implements RedheadDuck
+\{
+
+    public void display() {
+
+    }
+}
